54c7a05f01200b6fb04e5c9277f3911a860728ae
Tin Loc~ <trantinloc2005@gmail.com>
2025-02-14 23:54:42 +0700

    up sql

--- wedquanlysk/wedquanlysk/settings.py
+++ wedquanlysk/wedquanlysk/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = 'wedquanlysk.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "wedquanlysk",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
     }
 }
 
