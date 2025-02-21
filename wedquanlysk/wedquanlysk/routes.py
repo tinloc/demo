04d4603a531e18e09ee4e5e15282d4967892d986
Tin Loc~ <trantinloc2005@gmail.com>
2025-02-21 15:59:42 +0700

    up

--- wedquanlysk/wedquanlysk/routes.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .controllers import event_manager_view, home_view
-
-urlpatterns = [
-    path('', home_view, name='home'),
-    path('event_manager/', event_manager_view, name='event_manager'),
-]
